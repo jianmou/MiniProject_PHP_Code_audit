df13b6f257bea1f4fbfc817047b2daf092701aa4
System Administrator <root@192.168.124.3>
2017-12-27 14:29:01 +0800

    update

--- Web1/index.php
+++ Web1/index.php
@@ -1,14 +1,19 @@
-**Writeup**
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ZTS
+ * Date: 2017/12/15
+ * Time: 9:54
+ */
+    header("Content-Type: text/html;charset=utf-8");
+    $page=$_GET['page'];
+    if (isset($_GET['page'])){
+        include("$page");  //include 语句包含并运行指定文件。
+    }
+    else{
+        echo 'page!!!!';
+    }
 
-整体逻辑：
+    /*flag在Y29uZmln.php中，请使用本地文件包含方式获得flag*/
 
-
-考点：
-
-
-测试代码：
-
-Writeup：
-
-
-参考链接：
+?>
